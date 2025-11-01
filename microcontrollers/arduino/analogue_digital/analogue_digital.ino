eac3f8f76e2003b128aa7f0d3f8eae4f5bdb112f
william-j-m-m <w.j.m.mendez@gmail.com>
2025-11-01 23:33:02 +0000

    the final countdown (lie) (a+d)

--- microcontrollers/arduino/analogue_digital/analogue_digital.ino
+++ microcontrollers/arduino/analogue_digital/analogue_digital.ino
@@ -1,15 +1,22 @@
-int analogue_sensor_pentiometer = A0;
-int analogue_pentiometer_sensor_value = 0;
+int analogue_sensor_pentiometer_pin = A0;
+int analogue_sensor_pentiometer_value = 0;
+int digital_sensor_button_pin = 4;
+int digital_sensor_button_value = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(digital_sensor_button_pin, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogue_pentiometer_sensor_value = analogRead(analogue_sensor_pentiometer);
+  analogue_sensor_pentiometer_value = analogRead(analogue_sensor_pentiometer_pin);
   Serial.print("Joycon: ");
-  Serial.println(analogue_pentiometer_sensor_value);
+  Serial.println(analogue_sensor_pentiometer_value);
 
+  digital_sensor_button_value = digitalRead(digital_sensor_button_pin);
+  Serial.print("Button: ");
+  Serial.println(digital_sensor_button_value);
 }
