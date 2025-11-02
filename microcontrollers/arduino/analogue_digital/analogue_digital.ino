19a14de7c0505598c014b933142e9c42c2a373ca
william-j-m-m <w.j.m.mendez@gmail.com>
2025-11-02 00:03:36 +0000

    changed pins

--- microcontrollers/arduino/analogue_digital/analogue_digital.ino
+++ microcontrollers/arduino/analogue_digital/analogue_digital.ino
@@ -1,25 +1,27 @@
 #define NUM_ANALOGUE_PINS 1
-#define NUM_DIGITAL_PINS 1
+#define NUM_DIGITAL_PINS 3
 
-uint8_t analogue_pins[] = {A0, A1, A2, A3, A4};
+// List of Pins
+uint8_t analogue_pins[] = {A11, A12, A13, A14, A15};
 uint8_t digital_pins[] = {4, 5, 6, 7};
 
+// Define Array to Store Current Values
 int analogue_values[NUM_ANALOGUE_PINS];
 int digital_values[NUM_DIGITAL_PINS];
 
 void setup() {
-  // put your setup code here, to run once:
+  // Open Serial Link
   Serial.begin(9600);
 
+  // Set the mode for the digital pins
   for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
     pinMode(digital_pins[i], INPUT_PULLUP);
   }
-
-  // pinMode(digital_sensor_button_pin, INPUT_PULLUP);
 }
 
 void loop() {
   Serial.print("Analogue: {");
+  // Read and Update Values for all Analogue Pins
   for (int i = 0; i < NUM_ANALOGUE_PINS; i++) {
     analogue_values[i] = analogRead(analogue_pins[i]);
     Serial.print(analogue_values[i]);
@@ -28,6 +30,7 @@ void loop() {
   Serial.println("}");
 
   Serial.print("Digital: {");
+  // Read and Update Values for all Digital Pins
   for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
     digital_values[i] = digitalRead(digital_pins[i]);
     Serial.print(digital_values[i]);
